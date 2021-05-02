74ef9653c550f38054b7407a41d63c97c62b695c
Gokhan Egri <egrigokhan@gmail.com>
2021-05-02 18:11:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import pathlib
 from setuptools import setup, find_packages
 
 VERSION = '0.0.3' 
 DESCRIPTION = 'Pytorch Kernel Transformer'
 LONG_DESCRIPTION = 'Kernel attention implementation of Pytorch TransformerEncoderLayer'
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 # Setting up
 setup(
@@ -11,15 +18,21 @@ setup(
         version=VERSION,
         author="Gokhan Egri",
         author_email="<gegri@g.harvard.edu>",
+        url="https://github.com/egrigokhan/pykernsformer",
+        long_description=README,
+        long_description_content_type="text/markdown",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
         install_requires=["torch",
                           "numpy"],
+       license="MIT",
         
         keywords=['pytorch', 'transformer', 'attention'],
         classifiers= [
             "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
             "Intended Audience :: Education",
             "Programming Language :: Python :: 2",
             "Programming Language :: Python :: 3",
